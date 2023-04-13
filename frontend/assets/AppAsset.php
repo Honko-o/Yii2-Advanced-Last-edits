2df8e717c050fa48d248d8472ff8fc8dbac9688b
Saleh Abuhussein <salehabuhussein2@gmail.com>
2023-04-13 16:28:03 +0300

    Yii2 Advanced Template Commit

---
+++ frontend/assets/AppAsset.php
@@ -0,0 +1,23 @@
+<?php
+
+    namespace frontend\assets;
+
+    use \yii\web\AssetBundle;
+
+    class AppAsset extends AssetBundle {
+        public $basePath = "@webroot";
+        public $baseUrl = "@web";
+        public $css = [
+            YII_ENV_DEV ?
+                "node_modules/bootstrap/dist/css/bootstrap.css" :
+                "node_modules/bootstrap/dist/css/bootstrap.min.css",
+        ];
+        public $js = [
+            YII_ENV_DEV ?
+                "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" :
+                "node_modules/bootstrap/dist/js/bootstrap.bundle.js",
+        ];
+        public $depends = [
+            'yii\web\YiiAsset',
+        ];
+    }
---
+++ frontend/config/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+
+Yii::setAlias("@node_modules", dirname(__DIR__) . "/node_modules");
+
